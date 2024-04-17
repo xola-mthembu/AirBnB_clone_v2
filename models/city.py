b98430fa453129dec101baf4fa00db6f1f4ade10
Sihle <sihle.mtshali8@gmail.com>
2024-04-17 15:45:28 -0700

    8. DBStorage - Place (City)

--- models/city.py
+++ models/city.py
@@ -14,7 +14,9 @@ class City(BaseModel, Base):
         __tablename__ (str): The name of the MySQL table to store Cities.
         name (sqlalchemy String): The name of the City.
         state_id (sqlalchemy String): The state id of the City.
+        places (sqlalchemy relationship): The Place objects belonging to the City.
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
