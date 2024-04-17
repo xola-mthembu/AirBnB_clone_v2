8ad587aad82fc890bd15dda9a569e8d04d754294
xola-mthembu <xola.mthembu@gmail.com>
2024-04-17 12:59:54 -0700

    3. MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user hbnb_dev with password hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to hbnb_dev on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege to hbnb_dev on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
