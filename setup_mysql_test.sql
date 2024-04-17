8a40c2186fa50d59f9aa50977180db7ad4cc0e5d
xola-mthembu <xola.mthembu@gmail.com>
2024-04-17 13:01:12 -0700

    4. MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user hbnb_test with password hbnb_test_pwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege to hbnb_test on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
