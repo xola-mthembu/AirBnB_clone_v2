c11c2d97a919fb936d9637dfd151f55c55e2326f
Sihle <sihle.mtshali8@gmail.com>
2024-04-17 16:04:35 -0700

    10. DBStorage - Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the Amenity class."""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents an Amenity for a MySQL database.
+
+    Inherits from BaseModel and Base.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): The Amenity-Place relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place",
+                                   secondary="place_amenity",
+                                   back_populates="amenities")
