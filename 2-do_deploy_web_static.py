eef7849fa44d75144fe51ee13642bbad82c62b9e
xola-mthembu <xola.mthembu@gmail.com>
2024-05-03 17:59:03 -0700

    Task 2 updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
-"""
-Fabric script to deploy .tgz archive to web servers
-"""
-from fabric.api import put, run, env
-from os.path import exists
+"""Fabric script to distribute an archive to web servers"""
 
-# Update with actual server IPs
-env.hosts = ['54.162.233.113', '52.3.253.180']
-env.user = 'ubuntu'
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 def do_deploy(archive_path):
-    """Deploys an archive to web servers."""
-    if not exists(archive_path):
-        print(f"Archive not found: {archive_path}")
+    """Distributes an archive to web servers."""
+    if not os.path.exists(archive_path):
         return False
 
     try:
-        file_name = archive_path.split("/")[-1]
-        without_ext = file_name.split(".")[0]
-        target_path = f"/data/web_static/releases/{without_ext}/"
-
-        # Upload the archive
-        put_result = put(archive_path, "/tmp/")
-        if put_result.failed:
-            print("Failed to upload file")
-            return False
-
-        # Commands to unpack and setup the deployment
-        commands = [
-            f"mkdir -p {target_path}",
-            f"tar -xzf /tmp/{file_name} -C {target_path}",
-            f"rm /tmp/{file_name}",
-            f"mv {target_path}web_static/* {target_path}",
-            f"rm -rf {target_path}web_static",
-            "rm -rf /data/web_static/current",
-            f"ln -s {target_path} /data/web_static/current"
-        ]
-
-        for cmd in commands:
-            result = run(cmd)
-            if result.failed:
-                print(f"Command failed: {cmd}")
-                return False
-
-        print("Deployment successful")
+        filename = os.path.basename(archive_path)
+        no_ext = filename.split(".")[0]
+        path = "/data/web_static/releases/{}".format(no_ext)
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(path, path))
+        run("rm -rf {}/web_static".format(path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
-        print(f"Deployment failed: {e}")
+    except:
         return False
