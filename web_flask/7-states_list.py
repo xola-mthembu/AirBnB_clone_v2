c161eb2cc13c500a9d2b934b9e089852667513be
xola-mthembu <xola.mthembu@gmail.com>
2024-05-19 15:40:34 -0700

    Add 7-states_list.py to display list of states and template file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-/states_list: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
-UL tag: with the list of all State objects present in DBStorage
-sorted by name (A->Z)
-LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+Module to start a Flask web application
 """
 
 from flask import Flask, render_template
@@ -17,17 +10,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session."""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page with a list of states."""
-    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    """Displays a HTML page with a list of all State objects"""
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
